bb48c421817be47d64e5292205454b0260aa2613
SkimZ <70768898+SkimZconceited@users.noreply.github.com>
2021-09-22 21:13:33 +0300

    renamed and complete scrape

--- main.py
+++ main.py
@@ -1,46 +1,66 @@
-from bs4 import BeautifulSoup
-import requests
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
-driver = webdriver.Safari
-driver.get('https://tms.tpf.go.tz')
+PATH = "C:\Program Files (x86)\chromedriver.exe"
 
-# def get_page():
-#     url = f"https://tms.tpf.go.tz"
-#     plate_names(url)
+driver = webdriver.Chrome(PATH)
 
+num = "T 583 DEB"
 
-# def inputting():
-number = "T 658 DDA"
-inputElement = driver.find_element_by_class_name("vehicle")
-inputElement.send_keys(number.ENTER)
+driver.get("https://tms.tpf.go.tz/")
+print(driver.title)
 
-
-#  def plate_names(url):
-#     response = requests.get(url)
-
-#     html = response.content
-#     soup = BeautifulSoup(html, 'lxml')
-#     container = soup.find('section', class_ = 'container')
-#     title = container.find('div', class_ = 'list_header').text
-#     lists = container.find('div', class_ = 'list_wrap')
-#     com = lists.find('ul', class_ = 'directory-list active')
-#     lis_coms = com.find_all('li', class_ = 'provider provider-row')
-
-#     for lis_com in lis_coms:   #could not figure out how to for index, lis_com in enumerate(lis_coms): and make it count continuosly for the multiple pages
-#         compa = lis_com.find('h3', class_ = 'company_info')
-#         company = compa.find('a').text
-
-#         print(f'{company} \n')
-#     print('')
+search = driver.find_element_by_name("vehicle")
+search.send_keys(num)
+search.send_keys(Keys.RETURN)
 
 
+try:
+    tbdy = WebDriverWait(driver, 30).until(
+        EC.presence_of_element_located((By.TAG_NAME, "tbody"))   
+    )
 
-# if __name__ == '__main__':
-#     inputting()
+    trows = tbdy.find_elements_by_tag_name("tr")
+    
+    for trow in trows:
+        print(trow.text)
 
 
-# if __name__ == '__main__':
-    # for index, x in enumerate(range(0, 3)):
-        # get_page(x)
-        # plate_names()
+    #     theads = trow.find_elements_by_tag_name("th")
+    #     for thead in theads:
+    #         print(thead.text)
+
+    # for trow in trows:
+    #     tdets = trow.find_elements_by_tag_name("td")
+    #     for tdet in tdets:
+    #         print(tdets.text)
+               
+        
+
+        # n = 0
+        # while n < 12:
+        #     # m = n
+        #     # print(m)
+
+        #     print(thead[n].text)
+        #     print(tdets[n].text)
+
+        #     n = n+1
+
+finally:
+    driver.quit()
+
+
+
+# ref = driver.find_element_by_tag_name("th")
+# print(ref.text)
+
+# val = driver.find_element_by_tag_name("td")
+# print(val.text)
+
+# time.sleep(5)
+# driver.quit()

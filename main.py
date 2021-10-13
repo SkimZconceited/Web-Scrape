35092fe7bf0bc73e5f337b557a89715da77a1c91
SkimZ <70768898+SkimZconceited@users.noreply.github.com>
2021-10-14 00:17:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,31 +9,30 @@ PATH = "C:\Program Files (x86)\chromedriver.exe"
 
 driver = webdriver.Chrome(PATH)
 
-nums = ["T 580 DEB", "T 583 DEB"]
+plates = ['T 580 DEB', 'T 583 DEB', 'T 672 DJT']
+nums = "T 580 DEB"
 
 
 driver.get("https://tms.tpf.go.tz/")
 print(driver.title)
 
-for num in nums:
-
-    search = driver.find_element_by_name("vehicle")
-    search.send_keys(num)
-    search.send_keys(Keys.RETURN)
+search = driver.find_element_by_name("vehicle")
+search.send_keys(nums)
+search.send_keys(Keys.RETURN)
 
 
-    try:
-        tbdy = WebDriverWait(driver, 30).until(
-            EC.presence_of_element_located((By.TAG_NAME, "tbody"))   
-        )
+try:
+    tbdy = WebDriverWait(driver, 30).until(
+        EC.presence_of_element_located((By.TAG_NAME, "tbody"))   
+    )
 
-        trows = tbdy.find_elements_by_tag_name("tr")
-        
-        for trow in trows:
-            print(trow.text)
+    trows = tbdy.find_elements_by_tag_name("tr")
+    
+    for trow in trows:
+        print(trow.text)
 
-    finally:
-        driver.quit()
+finally:
+    driver.quit()
 
 # I just have to add a list that enters automatically after finishing one object
 # Create a word document after the results
